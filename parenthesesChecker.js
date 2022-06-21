2fb81dffcf81392bdad192d94f3c566804382a0a
kangcaleb <calebwkang@gmail.com>
2022-06-21 14:01:41 -0400

    first take at parentheses checker

--- parenthesesChecker.js
+++ parenthesesChecker.js
@@ -1,5 +1,20 @@
-
-
 const parenthesisChecker = (string) => {
 
+    const stack = [];
+    for(let i=0; i<string.length; i++) {
+        const char = string.charAt(i);
+
+        if (char == '(') {
+            stack.push(char);
+        } else if (char == ')') {
+            const removed = stack.pop();
+            if (removed === undefined) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length == 0;
 }
+
+console.log(parenthesisChecker(   "[()]{}{[()()]()}"  ));
