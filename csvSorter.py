e074fa3f8f509ade029a57993534c8fd4d3020d3
kangcaleb <calebwkang@gmail.com>
2022-06-22 15:07:12 -0400

    rest of the comments

--- csvSorter.py
+++ csvSorter.py
@@ -1,4 +1,7 @@
-import csv
-
-
 # Need an algorithm here
+
+# 1. Prompt for csv
+# 2. Read the header
+# 3. Create a dictionary to hold insuranceCompony -> [] of rows in record
+# 4. For each line in the csv, extract the insurance company. 
+#       - add it as a key to the dict if it doesn't exit. if it does. add it to the list
